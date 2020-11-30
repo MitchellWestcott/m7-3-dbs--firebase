4eb31f2c0b45e1de30bb4408217f10c4f3613763
MitchellWestcott <westcott.mitchell@gmail.com>
2020-11-30 13:06:18 -0500

    m7-3 complete

--- client/src/components/AppContext.js
+++ client/src/components/AppContext.js
@@ -1,9 +1,67 @@
-import React, { createContext, useEffect, useState } from 'react';
-
+import React, { createContext, useEffect, useState } from "react";
+import withFirebaseAuth from "react-with-firebase-auth";
+import * as firebase from "firebase";
+import "firebase/auth";
 export const AppContext = createContext(null);
 
-const AppProvider = ({ children }) => {
-  return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
+var firebaseConfig = {
+  apiKey: "AIzaSyCxK5gfzrxmngxs1KsRBolLeea7hdNaLSk",
+  authDomain: "web-app-40b7d.firebaseapp.com",
+  databaseURL: "https://web-app-40b7d.firebaseio.com",
+  projectId: "web-app-40b7d",
+  storageBucket: "web-app-40b7d.appspot.com",
+  messagingSenderId: "269048761383",
+  appId: "1:269048761383:web:a6a5d5279e6d4443d6d202",
+  measurementId: "G-F1296R051Z",
 };
 
-export default AppProvider;
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseAppAuth = firebaseApp.auth();
+
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
+
+const AppProvider = ({ children, signInWithGoogle, signOut, user }) => {
+  const [appUser, setAppUser] = useState({});
+  const [message, setMessage] = useState("");
+
+  const handleSignOut = () => {
+    signOut();
+    setAppUser({});
+  };
+
+  useEffect(() => {
+    if (user) {
+      fetch(`/users`, {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          displayName: user.displayName,
+          email: user.email,
+          photoURL: user.photoURL,
+        }),
+      })
+        .then((res) => res.json())
+        .then((json) => {
+          setAppUser(json.data);
+          setMessage(json.message);
+        });
+    }
+  }, [user]);
+
+  return (
+    <AppContext.Provider
+      value={{ appUser, signInWithGoogle, handleSignOut, message }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(AppProvider);
